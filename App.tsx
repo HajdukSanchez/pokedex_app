21db5f810f687562b6737b5ec836833354a92fa7
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-20 15:29:00 -0500

    🎉 [BEGIN] Start new project

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export { App };
