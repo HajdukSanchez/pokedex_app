21d0c85ef814ba0bfb51977389d66a45144e4bdc
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-22 15:52:06 -0500

    🚧 [ADD] Pokemon Screen

--- src/screens/pokemonScreen/PokemonScreen.styles.ts
+++ src/screens/pokemonScreen/PokemonScreen.styles.ts
@@ -2,7 +2,32 @@ import { StyleSheet } from 'react-native';
 import { globalStyles } from '../../theme/appTheme';
 
 export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    height: 370,
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+  },
   text: {
     ...globalStyles.text,
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    left: 20,
+  },
+  background: {
+    bottom: -60,
+    width: 250,
+    height: 250,
+    opacity: 0.7,
+  },
+  image: {
+    position: 'absolute',
+    bottom: -35,
   },
 });
--- src/screens/pokemonScreen/PokemonScreen.tsx
+++ src/screens/pokemonScreen/PokemonScreen.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image, useWindowDimensions } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
 import { StackScreenProps } from '@react-navigation/stack';
-import { RootStackParamList } from '../../routes/routes';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { styles } from './PokemonScreen.styles';
+import { toCapitalize } from '../../utilities/utils';
+import { RootStackParamList } from '../../routes/routes';
 
 interface PokemonScreenProps extends StackScreenProps<RootStackParamList, 'Pokemon'> {}
 
@@ -11,10 +15,27 @@ const PokemonScreen = ({
   route: {
     params: { pokemon, color },
   },
+  navigation: { navigate },
 }: PokemonScreenProps) => {
+  const { top } = useSafeAreaInsets();
+  const { width } = useWindowDimensions();
+
+  const handleBackButton = () => {
+    navigate('Home');
+  };
+
   return (
     <View>
-      <Text style={styles.text}>{pokemon.name}</Text>
+      <View style={{ ...styles.container, backgroundColor: color }}>
+        <TouchableOpacity activeOpacity={0.8} style={{ ...styles.backButton, top: top + 10 }} onPress={handleBackButton}>
+          <Icon name="arrow-back-outline" size={30} color={'white'} />
+        </TouchableOpacity>
+        <Text style={{ ...styles.text, top: top + 50 }}>
+          {toCapitalize(pokemon.name)} {'\n#' + pokemon.id}
+        </Text>
+        <Image source={require('../../assets/images/pokebola-blanca.png')} style={styles.background} />
+        <Image source={{ uri: pokemon.image }} style={{ ...styles.image, width: width * 0.75, height: width * 0.75 }} />
+      </View>
     </View>
   );
 };
